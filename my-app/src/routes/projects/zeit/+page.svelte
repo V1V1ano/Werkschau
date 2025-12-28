e0ead7caba2f9daf32f156a8c6f0fac9963eca0b
V1V1ano <vivien.serve@fh-potsdam.de>
2025-12-28 15:53:41 +0100

    add images Zeit

--- my-app/src/routes/projects/zeit/+page.svelte
+++ my-app/src/routes/projects/zeit/+page.svelte
@@ -23,7 +23,7 @@
     />
 
     <ImageAndText
-        imageSrc="https://i.pinimg.com/originals/2e/bb/01/2ebb017eb0feb6824440d3135713b002.jpg"
+        imageSrc="/project-images/ZEIT/teaser_quadrat.gif"
         body="During a six-month internship at ZEIT, my work focused on the intersection of journalism, design, and technology within the Data and Visualization Department. The core task was the design of interactive data articles that translated complex topics into accessible, mobile-first storytelling formats. Alongside developing interaction patterns and UI components beyond the existing corporate identity, the work included teaser visuals, editorial charts, interactive metrics, and graphic reviews. "
         chapter="summary"
         caption="A selection of feature images used for articles"
@@ -35,16 +35,16 @@
     />
 
     <ImageAndImage
-        leftSrc="https://wallpapers-clan.com/wp-content/uploads/2023/12/jujutsu-kaisen-grimacing-satoru-gojo-wallpaper-scaled.jpg"
+        leftSrc="/project-images/ZEIT/ruecktritte.webm"
         leftLabel="swipe interaction"
-        rightSrc="https://wallpapers.com/images/featured/gojo-satoru-pictures-8sz62hlsg1epni6l.jpg"
+        rightSrc="/project-images/ZEIT/abitur.webm"
         rightLabel="Quiz Mode"
     />
 
     <ImageAndText
         chapter="responsive illustrations"
         caption="A selection of responsive illustrations used in articles"
-        imageSrc="https://images.cgames.de/images/gsgp/290/jujutsu-kaisen-gojo-satoru_6307704.jpg"
+        imageSrc="/project-images/ZEIT/responsiveIllustrationen.gif"
         quote="Mobile traffic market share is the largest in November 2025 with 67.5% of all Mobile traffic"
         quoteSource="similar web. (2025). Mobile vs. Desktop vs. Tablet Traffic Market Share"
         body="Articles in ZEIT regularly feature explanatory illustrations. Since their target audience accesses these articles on a wide variety of devices, illustrations must be prepared for every use case. Therefore, I created infographic elements in dark mode, light mode, for mobile and desktop. And that's for a wide variety of topics: from mitochondrial replacement therapy to attacks on Iran."
@@ -53,7 +53,7 @@
     <ImageAndText
         chapter="mobile-friendly data storytelling"
         label="Project excerpt"
-        imageSrc="https://static0.cbrimages.com/wordpress/wp-content/uploads/2023/05/jujutsu-kaisen-gojo-223.jpg?w=1200&h=628&fit=crop"
+        imageSrc="/project-images/ZEIT/ZEIT-flow.png"
         body="Just as with illustrations, ZEIT articles focus on mobile-friendly data storytelling. This is particularly challenging when complex content and diverse interaction options should be conveyed. In this context, mobile first means not only reducing content to smaller screens, but also aligning narrative structures, interactions, and visual hierarchies from the beginning with shorter attention spans, vertical scrolling, and mobile intuitive usage. "
     />
 
