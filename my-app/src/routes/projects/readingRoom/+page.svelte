081c9ebef837f967012f6fffbff15d8bb29871d3
V1V1ano <vivien.serve@fh-potsdam.de>
2025-12-28 16:08:31 +0100

    add reading room content

--- my-app/src/routes/projects/readingRoom/+page.svelte
+++ my-app/src/routes/projects/readingRoom/+page.svelte
@@ -8,68 +8,55 @@
 
 <div class="project">
     <ProjectHero
-        keyArea="speculative"
-        title="Phygital Layers to Fragmented Realities"
-        subtitle="Speculative culturality rooted in immersive phygital technologies"
-        scope="Research · Concept · Storytelling · Design ·  Video production"
-        tools="Figma · various AI tool"
-        collaborators={[
-            "Leonidas von Bothmer",
-            "Natalia Pawlik",
-            "IPPSO @ UCLAB",
-        ]}
-        context={["University", "w/ Prof. Boris Müller", "Markus Kreutzer"]}
-        year="2024/25"
-        linkHref="https://uclab.fh-potsdam.de/ippso/"
-        linkLabel="IPPSO Project"
+        keyArea="narratives"
+        title="The City as a Reading Rooms"
+        subtitle="Collaborative exhibition about reading rooms in Potsdam"
+        scope="Data collection & presentation"
+        tools="Photogrammetry · Cutter · InDesign"
+        collaborators="Université de Strasbourg"
+        context={["University", "w/ Prof. Christina Poth", "Felix Walser"]}
+        year="2025"
+        linkHref="https://dujour.uber.space/thecityasareadingroom/"
+        linkLabel="Reading Rooms"
         mediaType="image"
-        mediaSrc="/project-images/Phygital/teaser.png"
+        mediaSrc="/project-images/ReadingRooms/big_picture.png"
     />
 
     <ImageAndText
-        imageSrc="https://i.pinimg.com/originals/2e/bb/01/2ebb017eb0feb6824440d3135713b002.jpg"
-        body={[
-            "Imagine a world a few decades from now where smartphones are a relict of the past and technology has become deeply immersive. Digital elements merge with physical experiences, altering how reality is perceived and understood. We call these enhancements phygital layers. We call this environment a phygital one.",
-            "This project explores how such technology reshapes communication and relationships. As individuals curate their own phygital layers, their realities become increasingly hyper-individual, which affects their perception and social interaction. Through a speculative narrative, the project highlights how beliefs and technological dependency emerge in personal, public and sensitive spaces. It asks questions about reality, responsibility, and communication.",
-        ]}
-        chapter="Project summary"
-        linkHref="https://www.youtube.com/watch?v=FCa8ZMvG0qM"
-        linkLabel="video documentation"
-        caption="Spaces restrict and enable technology"
+        imageSrc="/project-images/ReadingRooms/glue-on-the-floor.jpg"
+        body="This exhibition emerged from a collaboration with the Université de Strasbourg, during which reading spaces in Potsdam and Strasbourg were explored, documented, and discussed. The project critically reflects on the act of reading and the conditions that shape reading spaces, inviting visitors to immerse themselves in these environments and conversations. Participation is encouraged through a large floor map where visitors can add their own reading routes and locations, alongside a modular reading room designed for lingering. "
+        chapter="project summary"
+        caption="Visitors could mark their favourite reading room on the large map of Potsdam on the floor."
+        credit="©Alexander Molkenthin"
     />
 
-    <TextAndText
+     <ImageAndText
+        imageSrc="/project-images/ReadingRooms/3dScan.png"
+       body="We collected and cartographed reading rooms in Potsdam, experiencing their offerings and atmospheres firsthand. To preserve these spaces, we created 3D scans that were later exhibited. We also spent time sitting in the reading rooms and writing short texts, capturing lived, sparked, or imagined experiences from the perspectives of visitors or even the books themselves. These texts resulted in riso-printed bookmarks that visitors could take home from the exhibition."
         chapter="research"
-        leftQuote="In an era marked by rapid technological advancements, democracies face unprecedented challenges that transcend political institutions and reach into the fabric of everyday life."
-        leftQuoteSource="Altshuler et al. (2025). IPPSO–Transdisciplinary Research on Legal, Public Policy, and Design Perspectives for Immersive Phygital Public Spaces in Smart Cities."
-        rightBody="To explore people’s emotional responses to phygital technology and the socio-economic structures that result from it, we employed future wheel techniques, system mapping, and other speculative storytelling methods. To address potential social conflicts, we experimented with multiple perspectives and characters, each with a different level of reliance on phygital software. We drew inspiration from current attitudes towards smartphones and social media. Rather than focusing on extremes, we believe the most interesting insights lie in the nuances."
+        caption="3D Scan of one Reading Room"
+        credit="©Maurice Braatz & Me"
     />
 
-    <ImageAndImage
-        leftSrc="https://wallpapers-clan.com/wp-content/uploads/2023/12/jujutsu-kaisen-grimacing-satoru-gojo-wallpaper-scaled.jpg"
-        leftLabel="futures wheel"
-        leftCaption="The Futures Wheel is an explorative method used to structure and visualize direct and indirect effects of changes."
-        rightSrc="https://wallpapers.com/images/featured/gojo-satoru-pictures-8sz62hlsg1epni6l.jpg"
-        rightCaption="Different stakeholders rely on omnipresent sensory-altering techniques in different ways."
-    />
-
-    <TextAndText
+    <TextAndText 
+        leftHighlight="Everything can be a reading room. Everyone experiences reading differently: some seek solitude with a text, others connection through conversation, and some the desire to preserve what has been read."
         chapter="realisation"
-        leftHighlight="Person-to-person relationships are shaped by hyper-individual realities arising from personal phygital layers and social norms embedded in the space. To demonstrate this ambiguity, the narrative unfolds across three spaces: the personal, the public and the sensitive."
-        rightBody="We expressed our thought process in a video which uses parallel points of view. Even though our focus lies on person-to-person relationships, intra-personal, and person-to-space relationships overlap. A group of four old classmates, differenting greatly in their use and reliance on sensory altering technique, meet again. They start in the personal space of one person, moving through the street, which is an open space, and approach a senstive space, a cemetery. Conversations as well as their behaviour in these spaces demonstrates their fragmentation and conflict in finding common ground."
+        rightBody="The mapped reading spaces were made tangible in multiple ways. Participation was a central concern, which led to the creation of a large floor map of Potsdam where the documented reading spaces were located. Visitors were invited to place themselves and their own reading spaces onto the map. As a central element of the exhibition, the map was accompanied by images of the reading spaces and 3D scans. A particular highlight was a small reading room installed within the exhibition, where visitors could linger, exchange thoughts, read, and both retrace our experiences and add their own."
     />
 
-    <ImageAndImage
-        leftSrc="https://images.cgames.de/images/gsgp/290/jujutsu-kaisen-gojo-satoru_6307704.jpg"
-        leftCaption="In a phygital world, spaces are affected by, and affect, sensory-altering techniques."
-        rightSrc="https://static0.cbrimages.com/wordpress/wp-content/uploads/2023/05/jujutsu-kaisen-gojo-223.jpg?w=1200&h=628&fit=crop"
-        rightCaption="One scene is experienced sharply differently among the individuals of the story."
+    <ImageAndImage 
+    leftSrc="/project-images/ReadingRooms/reading-room.jpeg"
+    leftLabel="Reading room"
+    leftCredit="©ALEXANDER MOLKENTHIN"
+    rightSrc="/project-images/ReadingRooms/instructions-reading-room.jpg"
+    rightLabel="map instructions"
+    rightCredit="©ALEXANDER MOLKENTHIN"
     />
 
-    <TextAndText
+     <TextAndText 
+        leftHighlight="I want to make data available through a plethora of mediums and tools."
         chapter="reflection"
-        leftHighlight="I want to help people imagine and critically reflect futures."
-        rightBody="This project aims to spark debate around the legal dimensions: What rights do we have as senders and recipients of information? Can intent ever be guaranteed in how a message is perceived? And can we define a moral compass today that accounts for diverse beliefs, ethics, and legal frameworks? Throwing questions into the future and pulling them back into the present proved to be one of the most interesting challenges for me as a designer."
+        rightBody="Even though most of my work is situated in interface design, this project made clear a desire to move beyond the screen. It sparked an interest in designing more accessible experiences and working within participatory contexts. Visualization is not limited to digital interfaces; it can take on many different forms and exist across a wide range of spaces and interactions. "
     />
 
     <NextProjectTeaser
