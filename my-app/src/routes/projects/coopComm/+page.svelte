5196b49836fc5768d0af065f79e14d42bc10d2fb
V1V1ano <vivien.serve@fh-potsdam.de>
2025-12-28 15:09:16 +0100

    add texts coop

--- my-app/src/routes/projects/coopComm/+page.svelte
+++ my-app/src/routes/projects/coopComm/+page.svelte
@@ -8,68 +8,74 @@
 
 <div class="project">
     <ProjectHero
-        keyArea="speculative"
-        title="Phygital Layers to Fragmented Realities"
-        subtitle="Speculative culturality rooted in immersive phygital technologies"
-        scope="Research · Concept · Storytelling · Design ·  Video production"
-        tools="Figma · various AI tool"
-        collaborators={[
-            "Leonidas von Bothmer",
-            "Natalia Pawlik",
-            "IPPSO @ UCLAB",
+        keyArea="conceptual"
+        title="Co-op.Comm"
+        subtitle="Creating scalable semantic organigrams for co-operatives"
+        scope="Research · Concept · Design"
+        tools="Figma"
+        collaborators="10 co-operatives / collectives in Berlin / Potsdam"
+        context={[
+            "University",
+            "w/ Prof. Dr. Marian Dörk",
+            "Prof. Myriel Milicevic",
         ]}
-        context={["University", "w/ Prof. Boris Müller", "Markus Kreutzer"]}
-        year="2024/25"
-        linkHref="https://uclab.fh-potsdam.de/ippso/"
-        linkLabel="IPPSO Project"
+        year="2023"
+        linkHref="https://infovis.fh-potsdam.de/organigrams/"
+        linkLabel="Result website"
         mediaType="image"
-        mediaSrc="/project-images/Phygital/teaser.png"
+        mediaSrc="/project-images/CoopComm/teaser.gif"
     />
 
     <ImageAndText
         imageSrc="https://i.pinimg.com/originals/2e/bb/01/2ebb017eb0feb6824440d3135713b002.jpg"
         body={[
-            "Imagine a world a few decades from now where smartphones are a relict of the past and technology has become deeply immersive. Digital elements merge with physical experiences, altering how reality is perceived and understood. We call these enhancements phygital layers. We call this environment a phygital one.",
-            "This project explores how such technology reshapes communication and relationships. As individuals curate their own phygital layers, their realities become increasingly hyper-individual, which affects their perception and social interaction. Through a speculative narrative, the project highlights how beliefs and technological dependency emerge in personal, public and sensitive spaces. It asks questions about reality, responsibility, and communication.",
+            "Ten cooperatives from Potsdam and Berlin opened their doors to share how they organize working together. Beyond formal legal structures, cooperatives reveal their strength in situational responses to economic and social challenges – shaped by open and diverse communication cultures.",
+            "Co-op.comm makes these communication structures visible. It encodes communicational structures, rooms for communication, topic, members, and problems. It showcases the uniqueness of each organiation, questioning whether communication itself can function as an organizational chart. ",
         ]}
         chapter="Project summary"
-        linkHref="https://www.youtube.com/watch?v=FCa8ZMvG0qM"
-        linkLabel="video documentation"
-        caption="Spaces restrict and enable technology"
+        linkHref="https://fhp.incom.org/project/25346/open-to-public"
+        linkLabel="full written documentation"
+        caption="The exhibition poster showcases building blocks & examples"
+        credit="©Alexander Molkenthin"
     />
 
     <TextAndText
-        chapter="research"
-        leftQuote="In an era marked by rapid technological advancements, democracies face unprecedented challenges that transcend political institutions and reach into the fabric of everyday life."
-        leftQuoteSource="Altshuler et al. (2025). IPPSO–Transdisciplinary Research on Legal, Public Policy, and Design Perspectives for Immersive Phygital Public Spaces in Smart Cities."
-        rightBody="To explore people’s emotional responses to phygital technology and the socio-economic structures that result from it, we employed future wheel techniques, system mapping, and other speculative storytelling methods. To address potential social conflicts, we experimented with multiple perspectives and characters, each with a different level of reliance on phygital software. We drew inspiration from current attitudes towards smartphones and social media. Rather than focusing on extremes, we believe the most interesting insights lie in the nuances."
+        chapter="RESEARCH & Realisation"
+        leftHighlight="Members of co-operatives communicate with each other in interwoven, self-determined and fluid ways. Through this unique mode of communication, they jointly develop sustainable and subjective solutions."
+        rightBody={[
+            "The focus on communication emerged during discussions with individuals living and working in cooperative environments, where decisions were frequently described as processes that would be 'talked through'. Communication can be understood as a reflection of the cooperative structure itself. My design aims to highlight what distinguishes cooperative communication from the one in other work settings. It emphasises its intimacy through shared ownership and responsibility, and demonstrates that no cooperative functions in the same way as another.",
+            "Research revealed that cooperative communication is inherently multi-layered, spanning personal, work-related and cooperative-specific exchanges which often overlap across entities. These communicative layers are encoded as distinct visual variables within the visualization. Together, they form an organigrammatic structure that builds up subsequently.",
+        ]}
     />
-
-    <ImageAndImage
-        leftSrc="https://wallpapers-clan.com/wp-content/uploads/2023/12/jujutsu-kaisen-grimacing-satoru-gojo-wallpaper-scaled.jpg"
-        leftLabel="futures wheel"
-        leftCaption="The Futures Wheel is an explorative method used to structure and visualize direct and indirect effects of changes."
-        rightSrc="https://wallpapers.com/images/featured/gojo-satoru-pictures-8sz62hlsg1epni6l.jpg"
-        rightCaption="Different stakeholders rely on omnipresent sensory-altering techniques in different ways."
-    />
-
-    <TextAndText
-        chapter="realisation"
-        leftHighlight="Person-to-person relationships are shaped by hyper-individual realities arising from personal phygital layers and social norms embedded in the space. To demonstrate this ambiguity, the narrative unfolds across three spaces: the personal, the public and the sensitive."
-        rightBody="We expressed our thought process in a video which uses parallel points of view. Even though our focus lies on person-to-person relationships, intra-personal, and person-to-space relationships overlap. A group of four old classmates, differenting greatly in their use and reliance on sensory altering technique, meet again. They start in the personal space of one person, moving through the street, which is an open space, and approach a senstive space, a cemetery. Conversations as well as their behaviour in these spaces demonstrates their fragmentation and conflict in finding common ground."
-    />
-
-    <ImageAndImage
+     <ImageAndImage
         leftSrc="https://images.cgames.de/images/gsgp/290/jujutsu-kaisen-gojo-satoru_6307704.jpg"
-        leftCaption="In a phygital world, spaces are affected by, and affect, sensory-altering techniques."
+        leftCaption="Layers of communication in a co-operative from base to surface"
         rightSrc="https://static0.cbrimages.com/wordpress/wp-content/uploads/2023/05/jujutsu-kaisen-gojo-223.jpg?w=1200&h=628&fit=crop"
-        rightCaption="One scene is experienced sharply differently among the individuals of the story."
+        rightCaption="each visual variable corresponds to one layer of communication"
     />
+
+   
+    <TextAndText
+        chapter="final product"
+       leftQuote="The shift from exploration to narration puts ephemeral aspects of interactive visualizations up for debate and reflection, encouraging viewers to critically engage with data visualization. Conversely, shifting from narration to exploration challenges the authority of a singular story, enabling viewers to question predefined interpretations and construct their own pathways through the data."
+       leftQuoteSource="Morini et al. (2025). Critical Interactivity: Exploration and Narration in Data Visualization."
+       rightBody="The application offers two modes: explorative (dialogue view) and narrative (monologue view). These modes allow newcomers to learn the visual language and enable experienced users to navigate by tapping and filtering. Visual variables are enriched with tooltips that offer insight into communication spaces, challenges, and key topics.  It is realised as a website prototype encoding a preliminary set of co-operatives and collectives."
+       rightLinkLabel="Prototype"
+       rightLinkHref="https://www.figma.com/proto/d8CQHPhjl30T0BQ1ud5Z40/Unfolding-the-edges-of-communication?page-id=579%3A439053&node-id=588-445426&viewport=2015%2C-1270%2C0.66&t=S9UsaJn8HLkmGTQz-1&scaling=contain&content-scaling=fixed&starting-point-node-id=588%3A445426&show-proto-sidebar=1"
+    />
+
+     <ImageAndImage
+        leftSrc="https://wallpapers-clan.com/wp-content/uploads/2023/12/jujutsu-kaisen-grimacing-satoru-gojo-wallpaper-scaled.jpg"
+        leftLabel="monologue view"
+        rightSrc="https://wallpapers.com/images/featured/gojo-satoru-pictures-8sz62hlsg1epni6l.jpg"
+        rightLabel="Dialogue view"
+    />
+   
 
     <TextAndText
         chapter="reflection"
-        leftHighlight="I want to help people imagine and critically reflect futures."
-        rightBody="This project aims to spark debate around the legal dimensions: What rights do we have as senders and recipients of information? Can intent ever be guaranteed in how a message is perceived? And can we define a moral compass today that accounts for diverse beliefs, ethics, and legal frameworks? Throwing questions into the future and pulling them back into the present proved to be one of the most interesting challenges for me as a designer."
+        leftHighlight="I want to portray meaningful qualitative data through novel concepts."
+        rightBody="This project taught me that data visualization can be extremely valuable to the people it depicts. When their own experiences and stories are incorporated, it can help them reflect on and understand themselves. This project showcases my favorite mode of working: collecting data through meaningful encounters, taking a user-centric approach, and transcending classical forms of visualization."
     />
 
     <NextProjectTeaser
